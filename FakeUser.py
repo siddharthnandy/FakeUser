7dc23b3f2d1e7d2db1c13773200445cffabeb0ae
Siddharth Nandy <53063086+siddharthnandy@users.noreply.github.com>
2021-01-02 18:39:14 -0800

    Update FakeUser.py

--- FakeUser.py
+++ FakeUser.py
@@ -1,11 +1,3 @@
-"""
-The following code is used to replace every text by a user with a bot.
-The warrant for this was when someone changed his username to match that of another user.
-The bot was nicknamed to the user's actual name, such that when the bot replaced his texts,
-    the texts matched his name instead of his username (i.e. the second user's name).
-"""
-
-
 import discord
 import random
 from discord.ext import commands
@@ -16,10 +8,6 @@ client = commands.Bot(command_prefix = '.', intents = intents)
 @client.event
 async def on_ready():
     print('Bot is ready.')
-
-@client.command()
-async def ping(ctx):
-    await ctx.send(f'Pong! {round(client.latency * 1000)} ms')
 
 @client.event
 async def on_message(message):
